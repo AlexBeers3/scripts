372e5c7e2e300bf2c24557c8d64e398618ecdbf4
Alex Beers <abeers@customink.com>
2019-02-20 13:17:02 -0800

    change if...else to switch...case statement

--- split_to_CMYK.jsx
+++ split_to_CMYK.jsx
@@ -40,24 +40,28 @@ function splitChannels(targetChannel) {
           doc.selection.copy();
   }selectCopy();
       
-      if (targetChannel = 'Cyan') {
+      switch (targetChannel) {
+        case 'Cyan':
           doc.activeLayer = doc.artLayers.getByName("Cyan");
           doc.paste();
-          return
-    } else if (targetChannel = 'Magenta'){
+          break;
+        case 'Magenta':
           doc.activeLayer = doc.artLayers.getByName("Magenta");
           doc.paste();
-          return
-    } else if (targetChannel = 'Yellow'){
+          break;
+        case 'Yellow':
           doc.activeLayer = doc.artLayers.getByName("Yellow");
           doc.paste();
-          return
-    } else if (targetChannel = 'Black'){
+          break;
+        case 'Black':
           doc.activeLayer = doc.artLayers.getByName("Black");
           doc.paste();
-          return
-  } 
-}
+          break;
+        default:
+          doc.activeLayer = doc.artLayers.getByName("Background");
+    }
+} 
+
 splitChannels('Cyan');
 splitChannels('Magenta');
 splitChannels('Yellow');
@@ -67,82 +71,4 @@ splitChannels('Black');
 doc.changeMode(ChangeMode.RGB);
 
 //runs the adjustment layers action set
-//app.doAction("split_to_CMYK_adj_layers", "split_to_CMYK_adj_layers.ATN");
-
-
-
-//-------------------------------------------------------------------------------Old Code
-
-
-/*try {
-    if (app.documents.length) {
-      var layerColor = ["Cyan", "Magenta", "Yellow", "Black"];
-      var layers = doc.artLayers;
-      var cmyk_layers = [
-        layerK = layers.add(),
-        layerY = layers.add(),
-        layerM = layers.add(),
-        layerC = layers.add()
-      ];
-
-      if (layerColor) {
-        cmyk_layers[0].name = layerColor[3];
-        cmyk_layers[0].blendMode = BlendMode.NORMAL;
-        cmyk_layers[1].name = layerColor[2];
-        cmyk_layers[1].blendMode = BlendMode.MULTIPLY;
-        cmyk_layers[2].name = layerColor[1];
-        cmyk_layers[2].blendMode = BlendMode.MULTIPLY;
-        cmyk_layers[3].name = layerColor[0];
-        cmyk_layers[3].blendMode = BlendMode.MULTIPLY;
-
-      }
-
-      doc.activeLayer = newLayer;
-    }
-  } catch (e) {}
-}
-makeLayers();
-
-function selectCopy() {
-  doc.selection.selectAll();
-  doc.selection.copy();
-}
-
-//copies channels into corresponding layers
-function splitCyan() {
-  doc.activeLayer = doc.layers[4];
-  doc.activeChannels = [doc.channels.getByName('Cyan')];
-  doc.selection.selectAll();
-  doc.selection.copy();
-  doc.activeLayer = doc.layers[0];
-  doc.paste();
-}
-
-function splitMagenta() {
-  doc.activeLayer = doc.layers[4];
-  doc.activeChannels = [doc.channels.getByName('Magenta')];
-  doc.selection.selectAll();
-  doc.selection.copy();
-  doc.activeLayer = doc.layers[1];
-  doc.paste();
-}
-
-function splitYellow() {
-  doc.activeLayer = doc.layers[4];
-  doc.activeChannels = [doc.channels.getByName('Yellow')];
-  doc.selection.selectAll();
-  doc.selection.copy();
-  doc.activeLayer = doc.layers[2];
-  doc.paste();
-}
-
-function splitBlack() {
-  doc.activeLayer = doc.layers[4];
-  doc.activeChannels = [doc.channels.getByName('Black')];
-  doc.selection.selectAll();
-  doc.selection.copy();
-  doc.activeLayer = doc.layers[3];
-  doc.paste();
-}
-*/
-
+app.doAction("split_to_CMYK_adj_layers", "split_to_CMYK_adj_layers.ATN");
