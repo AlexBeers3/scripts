38c144b9c94c139eb12a453937e7992599b56f55
Alex Beers <abeers@customink.com>
2019-02-25 14:26:33 -0800

    no new changes to split_to_cmyk

--- split_to_CMYK.jsx
+++ split_to_CMYK.jsx
@@ -26,7 +26,7 @@ function makeLayers() {
 		var layerC = layers.add();
   			layerC.name = 'Cyan';
   			layerC.blendMode = BlendMode.MULTIPLY;
-  
+}  
   //used in splitChannels function
 function selectCopy() {
         doc.selection.selectAll();
@@ -51,3 +51,57 @@ doc.changeMode(ChangeMode.RGB);
 
 //runs the adjustment layers action set
 app.doAction("split_to_CMYK_adj_layers", "split_to_CMYK_adj_layers.ATN");
+
+//add hue sat adj layer
+var idMk = charIDToTypeID( "Mk  " );
+    var desc22 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref11 = new ActionReference();
+        var idAdjL = charIDToTypeID( "AdjL" );
+        ref11.putClass( idAdjL );
+    desc22.putReference( idnull, ref11 );
+    var idUsng = charIDToTypeID( "Usng" );
+        var desc23 = new ActionDescriptor();
+        var idNm = charIDToTypeID( "Nm  " );
+        desc23.putString( idNm, """Cyan color adjust""" );
+        var idGrup = charIDToTypeID( "Grup" );
+        desc23.putBoolean( idGrup, true );
+        var idType = charIDToTypeID( "Type" );
+            var desc24 = new ActionDescriptor();
+            var idpresetKind = stringIDToTypeID( "presetKind" );
+            var idpresetKindType = stringIDToTypeID( "presetKindType" );
+            var idpresetKindDefault = stringIDToTypeID( "presetKindDefault" );
+            desc24.putEnumerated( idpresetKind, idpresetKindType, idpresetKindDefault );
+            var idClrz = charIDToTypeID( "Clrz" );
+            desc24.putBoolean( idClrz, false );
+        var idHStr = charIDToTypeID( "HStr" );
+        desc23.putObject( idType, idHStr, desc24 );
+    var idAdjL = charIDToTypeID( "AdjL" );
+    desc22.putObject( idUsng, idAdjL, desc23 );
+executeAction( idMk, desc22, DialogModes.NO );
+
+//add levels adj layer
+var idMk = charIDToTypeID( "Mk  " );
+    var desc29 = new ActionDescriptor();
+    var idnull = charIDToTypeID( "null" );
+        var ref14 = new ActionReference();
+        var idAdjL = charIDToTypeID( "AdjL" );
+        ref14.putClass( idAdjL );
+    desc29.putReference( idnull, ref14 );
+    var idUsng = charIDToTypeID( "Usng" );
+        var desc30 = new ActionDescriptor();
+        var idNm = charIDToTypeID( "Nm  " );
+        desc30.putString( idNm, """Cyan Levels""" );
+        var idGrup = charIDToTypeID( "Grup" );
+        desc30.putBoolean( idGrup, true );
+        var idType = charIDToTypeID( "Type" );
+            var desc31 = new ActionDescriptor();
+            var idpresetKind = stringIDToTypeID( "presetKind" );
+            var idpresetKindType = stringIDToTypeID( "presetKindType" );
+            var idpresetKindDefault = stringIDToTypeID( "presetKindDefault" );
+            desc31.putEnumerated( idpresetKind, idpresetKindType, idpresetKindDefault );
+        var idLvls = charIDToTypeID( "Lvls" );
+        desc30.putObject( idType, idLvls, desc31 );
+    var idAdjL = charIDToTypeID( "AdjL" );
+    desc29.putObject( idUsng, idAdjL, desc30 );
+executeAction( idMk, desc29, DialogModes.NO );
